eb824f68ffc560fe36ae1b3c15efd33c6384d3ec
krypton <madhusudanbabar@gmail.com>
2021-09-08 20:13:42 +0530

    contact form done

--- app.js
+++ app.js
@@ -10,3 +10,14 @@ window.addEventListener("DOMContentLoaded", function () {
     speed: 0.2,
   });
 });
+
+function toggleNav(event) {
+  let bar = document.querySelector(".header");
+  if (window.pageYOffset > 35) {
+    bar.classList.add("scroll-down");
+  } else {
+    bar.classList.remove("scroll-down");
+  }
+}
+
+window.addEventListener("scroll", toggleNav);
